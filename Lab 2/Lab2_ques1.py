029305ad0ef3955882451f7476cf7c20b9f9686b
Prakhar Agrawal <144249231+prakharAg01@users.noreply.github.com>
2023-09-16 21:44:00 +0530

    Update Lab2_ques1.py

--- Lab 2/Lab2_ques1.py
+++ Lab 2/Lab2_ques1.py
@@ -1,1 +1,11 @@
+# input length and width of a plot and calculate its area
+#User input values
+L = float(input("Enter Length (in meters) : "))
+W = float(input("Enter width (in meters) : "))
+
+#calculation
+Area = L*W
+
+#Output
+print("Area of plot (in meter  square) is :",Area)
 
