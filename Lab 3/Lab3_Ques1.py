1298d3482877bb03a4d3e2b849c27ab98e5bffc0
Prakhar Agrawal <144249231+prakharAg01@users.noreply.github.com>
2023-09-16 21:46:11 +0530

    Create Lab3_Ques1.py

---
+++ Lab 3/Lab3_Ques1.py
@@ -0,0 +1,9 @@
+#Area of Trapezoid
+#user input
+X = float(input("Enter length of a side : "))
+Y = float(input("Enter length of another side : "))
+H = float(input("Enter Height : "))
+#using formula
+A = ((X+Y)/2)*H
+#output
+print("Area of Trapezoid is :",A)
